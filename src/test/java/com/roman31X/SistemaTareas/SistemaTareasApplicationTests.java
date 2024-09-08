ddc97d3b2d1bbd02469c02acaa54fa2aeed7a154
Roman31X <die.haa65@gmail.com>
2024-09-08 18:46:27 -0500

    Archivos creados

---
+++ src/test/java/com/roman31X/SistemaTareas/SistemaTareasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.roman31X.SistemaTareas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaTareasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
